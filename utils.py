edd5c733e9e1a92d9a5297ca77a0044659a876a0
Lars Martin Hodne <Lars.Martin.Hodne@norconsult.com>
2024-02-02 10:19:00 +0100

    Fix typo

--- utils.py
+++ utils.py
@@ -64,7 +64,7 @@ def start_task(task: int,  pop_size: int, gens: int, file_name: str = "", is_rou
     if file_name == "":
         assert 0 < task <= len(tasks), f"Du må velge enten oppgave 1, 2, 3, eller 4, ikke {task}"
     assert pop_size % 2 == 0, "For enkelhetsskyld bruker man hovedsaklig partall i POP_SIZE. Legg til 1 på POP_SIZE og prøv igjen."
-    assert pop_size <= 400, "Maks størrelse på populasjonen er 500"
+    assert pop_size <= 400, "Maks størrelse på populasjonen er 400"
     assert gens <= 300, "Maks antall generasjoner er 300"
 
     load_distances(task, file_name)
